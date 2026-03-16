bcacbebac76f5effcec1dc65d8dfdecff2ab48a8
jingtangg <jingtang1927@gmail.com>
2026-03-16 11:28:53 +0800

    chore:全域引入proj4

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+import 'ol/ol.css'
+import proj4 from 'proj4'
+import { register } from 'ol/proj/proj4'
+
+proj4.defs(
+  'EPSG:3826',
+  '+proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=GRS80 +units=m +no_defs'
+)
+register(proj4)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
